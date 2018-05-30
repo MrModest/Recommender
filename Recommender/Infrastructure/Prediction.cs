d4ad9f5db7360c1811d556039c37f598d14cb111
MrModest <babaevkamil@hotmail.com>
2018-05-30 19:20:04 +0300

    fix predictio

--- Recommender/Infrastructure/Prediction.cs
+++ Recommender/Infrastructure/Prediction.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
-using PredictionIONet.Clients;
 using Recommender.Models;
 
 namespace Recommender.Infrastructure
