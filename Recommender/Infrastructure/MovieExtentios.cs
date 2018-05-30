49eec1ea94bc5bc765c4b79dc86fffcd86c42106
MrModest <babaevkamil@hotmail.com>
2018-05-30 18:48:08 +0300

    comment PredictionIO

--- Recommender/Infrastructure/MovieExtentios.cs
+++ Recommender/Infrastructure/MovieExtentios.cs
@@ -9,39 +9,39 @@ namespace Recommender.Infrastructure
 {
     public static class MovieExtentios
     {
-        public static List<UserMovie> GetRecommendationByUserId(this IMovieRepository rep, int userId, int maxCount = 10)
-        {
-            var recommendations = Prediction.GetRecommendation(userId, maxCount);
-            var movies = rep.Movies.Where(m => recommendations.Select(r => r.ItemId).Contains(m.Id.ToString()));
-            var uMovies = new List<UserMovie>();
-            foreach (var item in recommendations)
-            {
-                var movie = movies.FirstOrDefault(m => m.Id.ToString() == item.ItemId);
-                uMovies.Add(new UserMovie{
-                    Movie = movie,
-                    PredictedScore = item.Score
-                });
-            }
+        //public static List<UserMovie> GetRecommendationByUserId(this IMovieRepository rep, int userId, int maxCount = 10)
+        //{
+        //    var recommendations = Prediction.GetRecommendation(userId, maxCount);
+        //    var movies = rep.Movies.Where(m => recommendations.Select(r => r.ItemId).Contains(m.Id.ToString()));
+        //    var uMovies = new List<UserMovie>();
+        //    foreach (var item in recommendations)
+        //    {
+        //        var movie = movies.FirstOrDefault(m => m.Id.ToString() == item.ItemId);
+        //        uMovies.Add(new UserMovie{
+        //            Movie = movie,
+        //            PredictedScore = item.Score
+        //        });
+        //    }
 
-            return uMovies;
-        }
+        //    return uMovies;
+        //}
 
-        public static async Task<List<UserMovie>> GetRecommendationByUserIdAsunc(this IMovieRepository rep, int userId, int maxCount = 10)
-        {
-            var recommendations = await Prediction.GetRecommendationAsync(userId, maxCount);
-            var movies = rep.Movies.Where(m => recommendations.Select(r => r.ItemId).Contains(m.Id.ToString()));
-            var uMovies = new List<UserMovie>();
-            foreach (var item in recommendations)
-            {
-                var movie = movies.FirstOrDefault(m => m.Id.ToString() == item.ItemId);
-                uMovies.Add(new UserMovie
-                {
-                    Movie = movie,
-                    PredictedScore = item.Score
-                });
-            }
+        //public static async Task<List<UserMovie>> GetRecommendationByUserIdAsunc(this IMovieRepository rep, int userId, int maxCount = 10)
+        //{
+        //    var recommendations = await Prediction.GetRecommendationAsync(userId, maxCount);
+        //    var movies = rep.Movies.Where(m => recommendations.Select(r => r.ItemId).Contains(m.Id.ToString()));
+        //    var uMovies = new List<UserMovie>();
+        //    foreach (var item in recommendations)
+        //    {
+        //        var movie = movies.FirstOrDefault(m => m.Id.ToString() == item.ItemId);
+        //        uMovies.Add(new UserMovie
+        //        {
+        //            Movie = movie,
+        //            PredictedScore = item.Score
+        //        });
+        //    }
 
-            return uMovies;
-        }
+        //    return uMovies;
+        //}
     }
 }
